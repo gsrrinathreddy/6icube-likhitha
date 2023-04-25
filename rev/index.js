97254bed24281e4856f0f77d0b03633c930149bb
nagasailikhitha <n.s.likhithach@gmail.com>
2023-04-25 12:16:23 +0530

    rev adding

---
+++ rev/index.js
@@ -0,0 +1,39 @@
+import express from "express";
+const app=express();
+app.get("/",(req,res)=>{
+    res.send("I have successfully completed my first API")
+});
+app.get("/Cosmetics",(req,res)=>{
+    res.json(
+        [
+            {
+                name:"eyelinear",
+                status:"false"
+            },
+              {
+                name:"kajal",
+                status:"false"
+            },
+               {
+                name:"bb cream",
+                status:"false"
+            },
+               {
+                name:"powder",
+                status:"false"
+            },
+               {
+                name:"lip stick",
+                status:"false"
+            },
+               {
+                name:"hair oil",
+                status:"false"
+            },
+        ]
+    )
+
+})
+app.listen(8002,()=>{
+    console.log("my server is running on port 8002")
+})
