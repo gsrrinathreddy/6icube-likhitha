187a3c4f79a8dca3f94ab72419fc321fa46b2a70
nagasailikhitha <n.s.likhithach@gmail.com>
2023-04-25 15:08:03 +0530

    cors adding

--- rev/index.js
+++ rev/index.js
@@ -1,5 +1,8 @@
 import express from "express";
+import cors from "cors"
+
 const app=express();
+app.use(cors())
 app.get("/",(req,res)=>{
     res.send("I have successfully completed my first API")
 });
